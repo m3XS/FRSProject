6ff15dfca437f539480870449780bc44d5ae1f98
Max <maxhammerschmidt9@gmail.com>
2021-05-28 12:19:53 +0200

    DET for 3 models now works

--- DETface.py
+++ DETface.py
@@ -5,36 +5,55 @@ from DET import DET
 
 from matplotlib import pyplot as plt
 
-#Files
-base = 'C:/Users/Max/PycharmProjects/frProject'
+local_path = os.getcwd()
 
-genuine_distance_file = os.path.join(base, 'genuine_scores_1vs1.txt')
-impostor_distance_file = os.path.join(base, 'impostor_scores_1vs1.txt')
+genuine_distance_file_facenet = os.path.join(local_path, 'distance/FaceNet/genuine_distance.txt')
+impostor_distance_file_facenet = os.path.join(local_path, 'distance/FaceNet/impostor_distance.txt')
+
+genuine_distance_file_vgg = os.path.join(local_path, 'distance/VGG-Face/genuine_distance.txt')
+impostor_distance_file_vgg = os.path.join(local_path, 'distance/VGG-Face/impostor_distance.txt')
+
+genuine_distance_file_openface = os.path.join(local_path, 'distance/OpenFace/genuine_distance.txt')
+impostor_distance_file_openface = os.path.join(local_path, 'distance/OpenFace/impostor_distance.txt')
 
 # Read scores we saved
-with open(genuine_distance_file, 'r') as f:
-    genuine_scores = numpy.asarray(f.read().splitlines())
+#FaceNet
+with open(genuine_distance_file_facenet, 'r') as f:
+    genuine_distance_facenet = numpy.asarray(f.read().splitlines())
 
-with open(impostor_distance_file, 'r') as f:
-    impostor_scores = numpy.asarray(f.read().splitlines())
+with open(impostor_distance_file_facenet, 'r') as f:
+    impostor_distance_facenet = numpy.asarray(f.read().splitlines())
+#VGG-Face
+with open(genuine_distance_file_vgg, 'r') as f:
+    genuine_distance_vgg = numpy.asarray(f.read().splitlines())
+
+with open(impostor_distance_file_vgg, 'r') as f:
+    impostor_distance_vgg = numpy.asarray(f.read().splitlines())
+#OpenFace
+with open(genuine_distance_file_openface, 'r') as f:
+    genuine_distance_openface = numpy.asarray(f.read().splitlines())
+
+with open(impostor_distance_file_openface, 'r') as f:
+    impostor_distance_openface = numpy.asarray(f.read().splitlines())
 
 # Create and configure figure
 det = DET(biometric_evaluation_type='algorithm', plot_title='FMR-FNMR')
 
-#det.x_limits = numpy.array([1e-4, .5])
-#det.y_limits = numpy.array([1e-4, .5])
-#det.x_ticks = numpy.array([1e-3, 1e-2, 5e-2, 20e-2, 40e-2])
-#det.x_ticklabels = numpy.array(['0.1', '1', '5', '20', '40'])
-#det.y_ticks = numpy.array([1e-3, 1e-2, 5e-2, 20e-2, 40e-2])
-#det.y_ticklabels = numpy.array(['0.1', '1', '5', '20', '40'])
+det.x_limits = numpy.array([1e-4, .5])
+det.y_limits = numpy.array([1e-4, .5])
+det.x_ticks = numpy.array([1e-3, 1e-2, 5e-2, 20e-2, 40e-2])
+det.x_ticklabels = numpy.array(['0.1', '1', '5', '20', '40'])
+det.y_ticks = numpy.array([1e-3, 1e-2, 5e-2, 20e-2, 40e-2])
+det.y_ticklabels = numpy.array(['0.1', '1', '5', '20', '40'])
 
 det.create_figure()
-det.plot(tar=genuine_scores, non=impostor_scores, label='distance')
+det.plot(tar=impostor_distance_facenet, non=genuine_distance_facenet, label='FaceNet')
+det.plot(tar=impostor_distance_vgg, non=genuine_distance_vgg, label='VGG-Face')
+det.plot(tar=impostor_distance_openface, non=genuine_distance_openface, label='OpenFace')
 det.legend_on()
 
 # Save DET plot
-det.save('example','png')
-
-
+det.save('DET', 'png')
+det.show()
 
 
